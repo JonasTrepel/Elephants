adbd7eade7c1c847c0af94fc1d5402114321afe8
Jonas Trepel <jonas.trepel@gmail.com>
2025-05-26 06:35:58 +0200

    buffer parks

--- R/cleaning/get_park_boundaries.R
+++ R/cleaning/get_park_boundaries.R
@@ -1,6 +1,7 @@
 library(sf)
 library(data.table)
 library(tidyverse)
+library(mapview)
 
 ### PARKS WITH LOCATION POINTS -------------------------------
 
@@ -130,13 +131,60 @@ pas_all[grepl("Mavinga", pas_all$NAME), ]
   #Makuya NR
   #uMkhuze - could be part of iSimangaliso
   #Venetia Limpopo
-  #Phongolo Game Reserve
+  #Phongolo Game Reserve --> not formerly protected
 
 
 pas_sub <- pas_all[pas_all$NAME %in% c(names_v_count), ] %>% 
   filter(WDPA_PID != 555705347)
 st_write(pas_sub, "data/spatial_data/protected_areas/park_boundaries.gpkg", append = FALSE)
 mapview(pas_sub)
+mapview(pas_sub[grepl("ZAF", pas_sub$ISO3), ])
 
-#not found: 
-#Makuya NR
+
+
+## create buffer of median elephant homerange diameter 
+
+dt_loc <- fread("data/processed_data/clean_data/all_location_data.csv") 
+
+dt_loc_nsa <- dt_loc %>% filter(!wdpa_pid %in% pas_all[pas_all$ISO3 == "ZAF", ]$WDPA_PID)
+dt_loc_sa <- dt_loc %>% filter(wdpa_pid %in% pas_all[pas_all$ISO3 == "ZAF", ]$WDPA_PID)
+
+unique(dt_loc_nsa$park_id)
+
+
+
+dt_hr <- dt_loc_nsa %>% 
+  dplyr::select(individual_id, hr_diameter_km, hr_mcp_area_km2) %>% 
+  unique() 
+
+dt_hr_sa <- dt_loc_sa %>% 
+  dplyr::select(individual_id, hr_diameter_km, hr_mcp_area_km2) %>% 
+  unique() 
+
+
+hist(dt_hr$hr_diameter_km)
+
+mean(dt_hr$hr_diameter_km, na.rm = T) # 87.35162
+median(dt_hr$hr_diameter_km, na.rm = T) # 72.53294
+
+mean(dt_hr_sa$hr_diameter_km, na.rm = T) # 50.76994
+median(dt_hr_sa$hr_diameter_km, na.rm = T) # 47.48037
+
+mean(dt_hr$hr_mcp_area_km2, na.rm = T) # 3931.655
+quantile(dt_hr$hr_mcp_area_km2, na.rm = T) # 2168.6491
+
+mean(dt_hr_sa$hr_mcp_area_km2, na.rm = T) # 1152.775
+quantile(dt_hr_sa$hr_mcp_area_km2, na.rm = T) # 1169.70026
+
+pas_buff_nsa <- pas_sub %>% 
+  filter(!ISO3 == "ZAF") %>% 
+  st_transform(crs = "ESRI:54009") %>% 
+  st_buffer(dist = mean(dt_loc$hr_diameter_km, na.rm = T)) #65
+
+pas_sa <- pas_sub %>% 
+  filter(ISO3 == "ZAF") %>% 
+  st_transform(crs = "ESRI:54009")
+
+pas_buff <- rbind(pas_buff_nsa,pas_sa)
+
+st_write(pas_buff, "data/spatial_data/protected_areas/park_boundaries_buffer_for_nsa.gpkg")
