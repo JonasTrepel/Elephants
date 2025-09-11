47910b3c75f3a917ed96e02b1502dab1d63d46fc
Jonas Trepel <au713983@uni.au.dk>
2025-09-11 18:03:42 +0200

    update home ranges plot

--- R/prep/3_process_covariates/rasterize_ecoregions.R
+++ R/prep/3_process_covariates/rasterize_ecoregions.R
@@ -4,7 +4,7 @@ library(sf)
 library(tidyverse)
 library(mapview)
 
-
+#https://ecoregions.appspot.com/
 sf_eco <- st_read("data/spatial_data/covariates/vector/ecoregions/Ecoregions2017.shp") %>% 
   st_transform(crs = "ESRI:54009")
 mapview(sf_eco, zcol = "ECO_NAME")
@@ -17,10 +17,18 @@ sf_eco %>%
   arrange(ECO_ID) %>%
   unique()
 
+sf_eco %>% 
+  as.data.frame() %>% 
+  mutate(geometry = NULL) %>% 
+  dplyr::select(BIOME_NUM, BIOME_NAME) %>% 
+  arrange(BIOME_NUM) %>%
+  unique()
+
+
 
 ### get raster of pa maps 
 
-temp_r_id <- rast(ext(sf_eco), resolution = 500, crs = "ESRI:54009")
+temp_r_id <- rast(ext(sf_eco), resolution = 1000, crs = "ESRI:54009")
 
 
 get_mode <- function(x, na.rm = TRUE) {
@@ -32,11 +40,24 @@ get_mode <- function(x, na.rm = TRUE) {
   return(ux[which.max(tabulate(match(x, ux)))])
 }
 
+
 eco_ids_r <- rasterize_polygons(sf_eco,
-                               temp_r_id)
+                       temp_r_id)
+
 
 eco_ids_r[] <- sf_eco$ECO_ID[eco_ids_r[]]
 
 plot(eco_ids_r)
 
-writeRaster(eco_ids_r, "data/spatial_data/covariates/raster/eco_regions_500m.tif")
+writeRaster(eco_ids_r, "data/spatial_data/covariates/raster/resolve_eco_regions_1km.tif")
+
+
+#now the same for biomes 
+biome_ids_r <- rasterize_polygons(sf_eco,
+                                temp_r_id)
+
+biome_ids_r[] <- sf_eco$BIOME_NUM[biome_ids_r[]]
+
+plot(biome_ids_r)
+
+writeRaster(biome_ids_r, "data/spatial_data/covariates/raster/resolve_biomes_1km.tif")
--- R/viz/plot_homeranges_and_step_summaries.R
+++ R/viz/plot_homeranges_and_step_summaries.R
@@ -1,5 +1,100 @@
 library(data.table)
 library(tidyverse)
+library(rnaturalearth)
+library(scico) 
+library(patchwork)
+
+##Homeranges ---------
+
+dt_ele_raw <- fread("data/processed_data/clean_data/elephant_id_meta_data.csv")
+
+dt_est <- fread("builds/model_outputs/issf_estimates_12hr_steps.csv") %>% 
+  left_join(dt_ele_raw) %>%
+  mutate(cluster_id = case_when(
+    cluster_id == "greater_kruger" ~ "Limpopo", 
+    cluster_id == "greater_waterberg" ~ "Limpopo", 
+    cluster_id == "limpopo" ~ "Limpopo", 
+    cluster_id == "kzn" ~ "KZN", 
+    cluster_id == "luangwa" ~ "Luangwa", 
+    cluster_id == "chobe" ~ "Chobe", 
+    cluster_id == "kafue" ~ "Kafue", 
+    cluster_id == "zambezi" ~ "Zambezi"
+  )) 
+
+dt_ele <- dt_ele_raw %>%
+  filter(individual_id %in% unique(dt_est$individual_id))
+
+sf_mcp <- st_read("data/spatial_data/elephants/mcp_home_ranges.gpkg") %>%
+  filter(individual_id %in% unique(dt_est$individual_id))
+
+# World 
+sf_world <- ne_countries(scale = "medium", returnclass = "sf")
+
+# Africa 
+sf_africa <- sf_world %>% filter(region_un == "Africa") %>% 
+  filter(!name == "Madagascar") %>% 
+  st_transform(., crs = 4326)
+
+### Plots ------------------------------------------
+p_1 <- sf_mcp %>% 
+  ggplot() +
+  ylim(-35, -7.5) +
+  xlim(9, 40) +
+  geom_sf(data = sf_africa, fill = "linen") +
+  geom_sf(alpha = 0.25, fill = "black") +
+  # geom_sf(data = sf_clust, alpha = 0.1, fill = "yellow") + 
+  theme_void()
+p_1
+
+
+p_2 <- dt_ele %>% 
+  filter(sex %in% c("M", "F")) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = sex, y = hr_mcp_area_km2, color = sex, fill = sex), alpha = 0.75) +
+  scale_color_scico_d(palette = "batlow") +
+  scale_fill_scico_d(palette = "batlow") +
+  labs(x = "Sex", y = "Home Range Area (km2)") +
+  theme(legend.position = "none", 
+        panel.grid.major.x = element_blank(), 
+        panel.grid.minor.x = element_blank(),
+        panel.border = element_blank(), 
+        panel.background = element_rect(fill = "snow"), 
+        strip.background = element_rect(fill = "linen", color = "linen"))
+p_2
+
+p_3 <- dt_ele %>% 
+  mutate(cluster_id = case_when(
+    cluster_id == "greater_kruger" ~ "Limpopo", 
+    cluster_id == "greater_waterberg" ~ "Limpopo", 
+    cluster_id == "limpopo" ~ "Limpopo", 
+    cluster_id == "kzn" ~ "KZN", 
+    cluster_id == "luangwa" ~ "Luangwa", 
+    cluster_id == "chobe" ~ "Chobe", 
+    cluster_id == "kafue" ~ "Kafue", 
+    cluster_id == "zambezi" ~ "Zambezi"
+  )) %>%
+  filter(cluster_id %in% c("Limpopo", "KZN", "Luangwa", "Chobe")) %>% 
+  filter(sex %in% c("M", "F")) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = sex, y = hr_mcp_area_km2, color = sex, fill = sex), alpha = 0.75) +
+  scale_color_scico_d(palette = "batlow") +
+  scale_fill_scico_d(palette = "batlow") +
+  labs(x = "Sex", y = "Home Range Area (km2)") +
+  facet_wrap(~cluster_id, scales = "free_y", ncol = 4) +
+  theme(legend.position = "none", 
+        panel.grid.major.x = element_blank(), 
+        panel.grid.minor.x = element_blank(),
+        panel.border = element_blank(), 
+        panel.background = element_rect(fill = "snow"), 
+        strip.background = element_rect(fill = "linen", color = "linen"))
+p_3
+
+library(patchwork)
+p_hr <- (p_1 | p_2) / p_3
+p_hr
+ggsave(plot = p_hr, "builds/plots/supplement/home_range_plot.png", dpi = 600, height = 8, width = 8)
+
+#### Step Summaries 
 
 dt_1 <- fread("data/processed_data/data_fragments/steps_1hr_incl_random.csv") %>% 
   filter(case_ == TRUE) %>% 
@@ -89,7 +184,7 @@ dt_1_sese <- dt_1 %>%
   ungroup() %>% 
   mutate(tier = paste0(season, "_", sex), 
          n_ids = n_distinct(individual_id))
-  
+
 dt_1_sum <- rbind(dt_1_all, dt_1_season, dt_1_sex, dt_1_sese) %>% 
   mutate(clean_tier = case_when(
     .default = tier,
@@ -271,15 +366,109 @@ dt_12_sum <- rbind(dt_12_all, dt_12_season, dt_12_sex, dt_12_sese) %>%
     step_time = "12hrs")
 
 
+# 24 hrs 
+
+dt_24_all <- dt_24 %>% 
+  group_by(individual_id) %>% 
+  summarize(median_sl = median(sl_km, na.rm = T), 
+            median_ta = median(ta_, na.rm = T), 
+            mean_sl = mean(sl_km, na.rm = T), 
+            mean_ta = mean(ta_, na.rm = T), 
+            q25_sl = quantile(sl_km, .25, na.rm = T), 
+            q25_ta = quantile(ta_, .25, na.rm = T), 
+            q75_sl = quantile(sl_km, .75, na.rm = T), 
+            q75_ta = quantile(ta_, .75, na.rm = T), 
+            n_steps = n()) %>% 
+  ungroup() %>% 
+  mutate(tier = "whole_year", 
+         n_ids = n_distinct(individual_id), 
+         sex = NA, 
+         season = NA)
+
+dt_24_season <- dt_24 %>% 
+  group_by(season, individual_id) %>% 
+  summarize(median_sl = median(sl_km, na.rm = T), 
+            median_ta = median(ta_, na.rm = T), 
+            mean_sl = mean(sl_km, na.rm = T), 
+            mean_ta = mean(ta_, na.rm = T), 
+            q25_sl = quantile(sl_km, .25, na.rm = T), 
+            q25_ta = quantile(ta_, .25, na.rm = T), 
+            q75_sl = quantile(sl_km, .75, na.rm = T), 
+            q75_ta = quantile(ta_, .75, na.rm = T), 
+            n_steps = n()) %>% 
+  ungroup() %>% 
+  mutate(tier = season, 
+         n_ids = n_distinct(individual_id), 
+         sex = NA)
+
+dt_24_sex <- dt_24 %>% 
+  filter(sex %in% c("M", "F")) %>% 
+  group_by(sex, individual_id) %>% 
+  summarize(median_sl = median(sl_km, na.rm = T), 
+            median_ta = median(ta_, na.rm = T), 
+            mean_sl = mean(sl_km, na.rm = T), 
+            mean_ta = mean(ta_, na.rm = T), 
+            q25_sl = quantile(sl_km, .25, na.rm = T), 
+            q25_ta = quantile(ta_, .25, na.rm = T), 
+            q75_sl = quantile(sl_km, .75, na.rm = T), 
+            q75_ta = quantile(ta_, .75, na.rm = T), 
+            n_steps = n()) %>% 
+  ungroup() %>% 
+  mutate(tier = sex, 
+         n_ids = n_distinct(individual_id), 
+         season = NA)
+
+dt_24_sese <- dt_24 %>% 
+  filter(sex %in% c("M", "F")) %>% 
+  group_by(sex, season, individual_id) %>% 
+  summarize(median_sl = median(sl_km, na.rm = T), 
+            median_ta = median(ta_, na.rm = T), 
+            mean_sl = mean(sl_km, na.rm = T), 
+            mean_ta = mean(ta_, na.rm = T), 
+            q25_sl = quantile(sl_km, .25, na.rm = T), 
+            q25_ta = quantile(ta_, .25, na.rm = T), 
+            q75_sl = quantile(sl_km, .75, na.rm = T), 
+            q75_ta = quantile(ta_, .75, na.rm = T), 
+            n_steps = n()) %>% 
+  ungroup() %>% 
+  mutate(tier = paste0(season, "_", sex), 
+         n_ids = n_distinct(individual_id))
+
+dt_24_sum <- rbind(dt_24_all, dt_24_season, dt_24_sex, dt_24_sese) %>% 
+  mutate(clean_tier = case_when(
+    .default = tier,
+    tier == "whole_year"     ~ "Whole Year\nAll Individuals", 
+    tier == "dry_season"     ~ "Dry Season\nAll Individuals",
+    tier == "wet_season"     ~ "Wet Season\nAll Individuals",
+    tier == "F"              ~ "Whole Year\nFemales",
+    tier == "M"              ~ "Whole Year\nMales",
+    tier == "dry_season_F"   ~ "Dry Season\nFemales",
+    tier == "wet_season_F"   ~ "Wet Season\nFemales",
+    tier == "dry_season_M"   ~ "Dry Season\nMales",
+    tier == "wet_season_M"   ~ "Wet Season\nMales"), 
+    step_time = "24hrs")
+
+
 # summarize 
-dt_sum <- rbind(dt_1_sum, dt_3_sum, dt_12_sum)
+dt_sum <- rbind(dt_1_sum, dt_3_sum, dt_12_sum, dt_24_sum)
+
+library(scico)
 
 p_sum <- dt_sum %>% 
+  mutate(step_time = factor(step_time, levels = c("1hr", "3hrs", "12hrs", "24hrs"))) %>% 
   ggplot() +
-  geom_boxplot(aes(y = median_sl, x = clean_tier)) +
+  geom_boxplot(aes(y = median_sl, x = clean_tier, fill = clean_tier, color = clean_tier), alpha = 0.75) +
   labs(y = "Median Step Length (km)", x = "") +
   facet_wrap(~step_time, scales = "free_y", ncol = 1) +
-  theme_bw()
+  scale_fill_scico_d(palette = "batlow") +
+  scale_color_scico_d(palette = "batlow") +
+  theme(legend.position = "none", 
+        panel.grid.major.x = element_blank(), 
+        panel.grid.minor.x = element_blank(),
+        panel.border = element_blank(), 
+        panel.background = element_rect(fill = "snow"), 
+        strip.background = element_rect(fill = "linen", color = "linen"))
+
 p_sum
 
-ggsave(plot = p_sum, "builds/plots/supplement/step_length_distribution.png", dpi = 600, height = 9, width = 9)
+ggsave(plot = p_sum, "builds/plots/supplement/step_length_distribution.png", dpi = 600, height = 10, width = 9)
